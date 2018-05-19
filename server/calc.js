68613bfab10b0b96318faccdc801de716f0e9701
David Kumarbai <cygnidavid@gmail.com>
2018-05-19 13:40:27 -0700

    server/calc.js: add script to calculate distance

---
+++ server/calc.js
@@ -0,0 +1,21 @@
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 3959;
+  var dLat = deg2rad(lat2-lat1);
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return Math.ceil(d);
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+//37.418482, -122.059392
+//37.405995, -121.984231
+const dist = getDistanceFromLatLonInKm(37.423965, -122.056387, 37.408116, -121.759242);
+console.log("dist is", dist)
