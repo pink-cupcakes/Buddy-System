5d8aa2ae9b6101f31b5e56064cc6792abf3c621f
David Kumarbai <cygnidavid@gmail.com>
2018-05-19 20:45:00 -0700

    dummy data

--- server/app.js
+++ server/app.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded());
 app.use(express.json());  
 app.use(express.static(`${__dirname}./../dist`));
 
-app.get("/buddies/:id", (req, resp) => {
+// app.get("/buddies/:id", (req, resp) => {
+app.get("/buddies/jPs49", (req, resp) => {
   // Look up database for the id param
   const { id } = req.body.id;
   const data = { 
@@ -33,16 +34,16 @@ app.get("/buddies/:id", (req, resp) => {
 
 app.get("/catastrophie", (req, resp) => {
   client.messages
-  .create({
-     body: 'http://localhost:3000/buddies/e43jwaK',
-     from: '+19519014443',
-     to: '+16287778666'
-   })
-  .then(message => {
-    console.log(message.sid)
-    resp.end()
-  })
-  .done();
+    .create({
+      body: 'http://localhost:3000/buddies/jPs49',
+      from: '+19519014443',
+      to: '+16287778666'
+    })
+    .then(message => {
+      console.log(message.sid)
+      resp.end()
+    })
+    .done();
 });
 
 app.post("/user_signup", (req, resp) => {
@@ -57,7 +58,7 @@ app.post("/user_signup", (req, resp) => {
 });
 
 app.get("*", (req, resp) => {
-  resp.sendFile(__dirname, "../client/static/index.html");
+  resp.sendFile(__dirname, "../dist/index.html");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- server/calc.js
+++ server/calc.js
@@ -1,21 +1,18 @@
-function getDistance(lat1,lon1,lat2,lon2) {
-  var R = 3959;
-  var dLat = deg2rad(lat2-lat1);
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
+const getDistance = (lat1,lon1,lat2,lon2) => {
+  let R = 3959;
+  let dLat = deg2rad(lat2-lat1);
+  let dLon = deg2rad(lon2-lon1); 
+  let a = 
     Math.sin(dLat/2) * Math.sin(dLat/2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c;
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  let d = R * c;
   return Math.ceil(d);
-}
+};
 
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
+const deg2rad = (deg) => deg * (Math.PI/180);
 
-const dist = getDistance(37.423965, -122.056387, 37.408116, -121.759242);
+// const dist = getDistance(37.423965, -122.056387, 37.408116, -121.759242);
 
 module.exports.getDistance = getDistance
